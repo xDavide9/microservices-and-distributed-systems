f1a080491c7d5872e047906169cd82532a041988
xdavide9 <biancodavidetech@gmail.com>
2024-08-04 13:33:31 +0200

    customer
    created microservice customer following n-tier architecture, controller accepts a simple post request to register a new customer

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,20 @@
+package com.amigoscode.customer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        customerRepository.save(customer);
+    }
+}
